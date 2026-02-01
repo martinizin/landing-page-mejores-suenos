4badd7e8f691c3845ce10b4d4d9266ca3324ab06
martinizin <mar45pillacela@gmail.com>
2026-01-31 19:16:42 -0500

    Cambios en astro config v3

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import vercel from '@astrojs/vercel';
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   output: 'server',
